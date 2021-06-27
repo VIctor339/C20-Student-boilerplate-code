b17711beaa02248d104adf614c290c56cffdb0b3
VIctor339 <84278922+VIctor339@users.noreply.github.com>
2021-06-27 09:58:22 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,9 +8,11 @@ let engine;
 let world;
 var ball;
 var ground;
+var rock;
 
 function setup() {
   createCanvas(400,400);
+  
 
   engine = Engine.create();
   world = engine.world;
@@ -24,12 +26,20 @@ function setup() {
      isStatic: true
    };
   
+  var rock_options = {
+    restitution: 0.85,
+    frictionAir:0.01
+  }
+  
 
   ground = Bodies.rectangle(200,390,400,20,ground_options);
   World.add(world,ground);
 
   ball = Bodies.circle(100,10,20,ball_options);
   World.add(world,ball);
+  
+  rock = Bodies.circle(300,20,10,rock_options);
+ World.add(world,rock);
   
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -43,6 +53,9 @@ function draw()
   
   ellipse(ball.position.x,ball.position.y,20);
   rect(ground.position.x,ground.position.y,400,20);
+  
+  ellipse(rock.position.x,rock.position.y,10);
+  
  
 }
 
